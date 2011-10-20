7f098f0d006ef157878e023ff1ab61315648bd12
Mateusz Loskot <mateusz@loskot.net>
2011-10-20 23:17:55 +0100

    Added simple project based on GNU autotools to enable quick testing of Autoconf modules in /macros folder.

---
+++ test/src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Sample program may be used to test if the Autoconf macros
+// successfully determine if required C/C++ libraries are available.
+// 
+// Author: Mateusz Łoskot 2011 <mateusz@loskot.net>
+//
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
